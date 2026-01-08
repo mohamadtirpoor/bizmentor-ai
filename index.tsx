18e56ef198509672192088c4ea1186c5fd7c9cae
mohamadtirpoor <mohamad.tirpoor@gmail.com>
2026-01-08 15:07:32 +0330

    Add OpenReplay tracker

--- index.tsx
+++ index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Clarity from '@microsoft/clarity';
+import Tracker from '@openreplay/tracker';
 import App from './App';
 import './index.css';
 
 // Initialize Microsoft Clarity
 Clarity.init('ux87vyobxr');
+
+// Initialize OpenReplay Tracker
+const tracker = new Tracker({
+  projectKey: "ABMWUNTeeNqb0iAELqAN",
+  ingestPoint: "https://openreplay.doctordaraei.com/ingest",
+});
+tracker.start();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
